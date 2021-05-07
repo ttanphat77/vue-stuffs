395eae7e39671083513ad7b085c20402d61799b3
phat <ttanphat77@gmail.com>
2021-05-07 17:10:01 +0700

    init

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,143 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <!-- <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title>
+          Quasar App
+        </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header> -->
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered class="bg-grey-1">
+      <div class="col-12" style="height: 200px">
+        <q-img
+          src="https://cdn.hipwallpaper.com/i/65/73/ObXPkE.jpg"
+          style="height: 120px"
+          class="profile-container"
+        >
+          <div class="absolute-full"></div>
+          <div class="profile-info bg-transparent">
+            <q-avatar size="130px" class="q-mb-sm profile-avatar">
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+            </q-avatar>
+            <div class="text-weight-bold">Trần Tấn Phát</div>
+            <div>ttanphat77@gmail.com</div>
+          </div>
+        </q-img>
+      </div>
+      <q-list>
+        <q-item-label header class="text-grey-8">
+          Essential Links
+        </q-item-label>
+
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<style lang="scss">
+.profile-container {
+  overflow: visible;
+  .profile-info {
+    text-align: center;
+    color: #000;
+    width: 100%;
+    .profile-avatar {
+      img {
+        border: 5px solid white;
+      }
+    }
+  }
+}
+</style>
+
+<script lang="ts">
+import EssentialLink from 'components/EssentialLink.vue';
+
+const linksList = [
+  {
+    title: 'Docs',
+    caption: 'quasar.dev',
+    icon: 'school',
+    link: 'https://quasar.dev',
+  },
+  {
+    title: 'Github',
+    caption: 'github.com/quasarframework',
+    icon: 'code',
+    link: 'https://github.com/quasarframework',
+  },
+  {
+    title: 'Discord Chat Channel',
+    caption: 'chat.quasar.dev',
+    icon: 'chat',
+    link: 'https://chat.quasar.dev',
+  },
+  {
+    title: 'Forum',
+    caption: 'forum.quasar.dev',
+    icon: 'record_voice_over',
+    link: 'https://forum.quasar.dev',
+  },
+  {
+    title: 'Twitter',
+    caption: '@quasarframework',
+    icon: 'rss_feed',
+    link: 'https://twitter.quasar.dev',
+  },
+  {
+    title: 'Facebook',
+    caption: '@QuasarFramework',
+    icon: 'public',
+    link: 'https://facebook.quasar.dev',
+  },
+  {
+    title: 'Quasar Awesome',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev',
+  },
+];
+
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    EssentialLink,
+  },
+
+  setup() {
+    const leftDrawerOpen = ref(false);
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+  },
+});
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,51 @@
+<template>
+  <q-page class="row">
+    <!-- <example-component
+      title="Example component"
+      active
+      :todos="todos"
+      :meta="meta"
+    ></example-component> -->
+    <bitcoin></bitcoin>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { Todo, Meta } from 'components/models';
+import ExampleComponent from 'components/CompositionComponent.vue';
+import Bitcoin from 'components/Bitcoin.vue';
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'PageIndex',
+  components: { ExampleComponent, Bitcoin },
+  // setup() {
+  //   const todos = ref<Todo[]>([
+  //     {
+  //       id: 1,
+  //       content: 'ct1',
+  //     },
+  //     {
+  //       id: 2,
+  //       content: 'ct2',
+  //     },
+  //     {
+  //       id: 3,
+  //       content: 'ct3',
+  //     },
+  //     {
+  //       id: 4,
+  //       content: 'ct4',
+  //     },
+  //     {
+  //       id: 5,
+  //       content: 'ct5',
+  //     },
+  //   ]);
+  //   const meta = ref<Meta>({
+  //     totalCount: 1200,
+  //   });
+  //   return { todos, meta };
+  // },
+});
+</script>
